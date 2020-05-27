e4ee5d78be6e77e31b98e15af9aba43012012986
jmarcon <juliano.marcon@gmail.com>
2020-05-27 18:00:59 +0000

    Release 20200527.2 by Azure DevOps

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,32 @@
 hljs.initHighlightingOnLoad();
+
+let isDark = false;
+function loadTheme() {
+    
+    var css = document.getElementById("myStyle");
+    let theme = localStorage.getItem("theme");
+    if(!theme){
+        theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
+        localStorage.setItem("theme", theme);
+    }
+
+    css.theme = theme;
+    if(theme === "dark") {
+        css.href = "/assets/css/maindark.css";
+    } else {
+        css.href = "/assets/css/main.css";
+    }
+}
+
+loadTheme();
+
+function cssToggle(e) {
+    e.preventDefault();
+    let theme = localStorage.getItem("theme");
+    if(theme === "dark") {
+        localStorage.setItem("theme", "light");
+    }else{
+        localStorage.setItem("theme", "dark");
+    }
+    loadTheme()
+}
